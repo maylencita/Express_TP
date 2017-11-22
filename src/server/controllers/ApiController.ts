63a0c94dbf765a2afd34add7316687074e5dd502
Morgane <me@kreatize.com>
2017-11-22 14:00:23 +0100

    API controller functions

--- src/server/controllers/ApiController.ts
+++ src/server/controllers/ApiController.ts
@@ -1,5 +1,5 @@
 import { Response, Request } from "express";
-import { User, Channel } from '../models'; 
+import { User, Channel, Message, Question, Reponse, Note } from '../models'; 
 import store from '../models/store'
 
 export function addUser(request: Request, response: Response){
@@ -16,15 +16,133 @@ export function createChannel(request: Request, response: Response){
   response.send();
 } 
 
- //Crée un chanel sans participants. Vérifier points >= 1
-// export function inviteUser(user: User, contexte: {}, nomChannel: string, upserPseudo: string) {} //Vérifier user is owner ou points >= 2; Ajoute un utilisateur à un channel
-// export function readChannel //Vérifier appartennance 
-// export function sendQuestion(nomChannel, message) //Vérifier appartennance; if points ==0 then add 1 point to user
-// export function sendAnswer(nomChannel, message) //Vérifier appartennance ou super user && points >= 3
-// export function noteQuestion(...) //Vérifier appartenance && points >= 1 ; récalculer points
-// export function noteAnswer(...) //Vérifier appartenance && points >= 2 ; récalculer points
-// export function suspendUser(userPseudo) // only super users (5 points) peuvent suspendre un utilisateur
-// export function calculerPoints // selon la notation de ces questions / responses
+// export function inviteUser(request: Request, response: Response){
+//   var channel:Channel = store.state.channels.find( (channel) =>{
+//     return channel.nom === request.body.nomChannel
+//   });
+//   if (channel !== undefined){
+//     var user:User = channel.participants.find( (user) =>{
+//       return user.pseudo === request.body.userPseudo
+//     });
+
+//     if (user !== undefined && (channel.createur===user||user.points>=2)){
+//       channel.participants.push();
+//     }
+//   }
+//   response.send();
+// }
+  
+
+// 	if(index!==-1)
+// 	{
+// 		serveur.channels[indexChannel].participants.push(newUser);
+// 	}
+// 	return serveur;
+// }
+//   (user: User, contexte: {}, nomChannel: string, upserPseudo: string) {} //Vérifier user is owner ou points >= 2; Ajoute un utilisateur à un channel
+
+
+// request body : 
+// channel : Channel 
+// user : User
+export function readChannel(request: Request, response: Response){
+  let messages:Message[]  = []; 
+	if(request.body.channel.participants.indexOf(request.body.user)!==-1)
+	{
+		for(let id in request.body.channel.messages)
+		{
+			messages.push(store.state.messages[id]);
+		}
+	}
+  response.send(messages);
+} 
+
+// message : Message
+export function sendQuestion(request: Request, response: Response){  
+  const question:Question = request.body.message;
+	
+  for(let channel in question.destination){
+    if(question.destination[channel].participants.indexOf(question.emetteur)!==-1)
+    {
+    if(store.state.users[store.state.users.indexOf(question.emetteur)].points===0)
+    {
+      store.state.users[store.state.users.indexOf(question.emetteur)].points++;
+    }
+      store.state.channels[store.state.channels.indexOf(question.destination[channel])].messages.push(question.id);
+    }
+  }
+  store.state.messages.push(question);
+  response.send();
+}
+
+// message : Message
+// nomChannel : String 
+export function sendAnswer(request: Request, response: Response){  
+  const reponse:Reponse = request.body.message;
+  const sourceMessage:Message = store.state.messages[reponse.idQuestion];
+  const channel: Channel = store.state.channels.find((channel) =>{
+    return channel.nom === request.body.nomChannel
+  });
+  if((channel.participants.indexOf(reponse.emetteur)!==-1 && reponse.emetteur.points>=3)||reponse.emetteur.points===5)
+	{
+		store.state.messages.push(reponse);
+		store.state.channels[store.state.channels.indexOf(channel)].messages.push(reponse.id);
+	}
+  response.send();
+}
+
+// note : Note
+// channelName : String
+export function noteQuestion(request: Request, response: Response){  
+  const note:Note = request.body.note;
+  const channel: Channel = store.state.channels.find((channel) =>{
+    return channel.nom === request.body.nomChannel
+  });
+  if(channel.participants.indexOf(note.emetteur)!==-1 && note.emetteur.points>=1)
+	{
+		store.state.messages.push(note);
+		store.state.channels[store.state.channels.indexOf(channel)].messages.push(note.id);
+		calculerPoints(note.emetteur);
+	}
+  response.send();
+}
+
+// note : Note
+// channelName : String
+export function noteAnswer(request: Request, response: Response){  
+  const note:Note = request.body.note;
+  const channel: Channel = store.state.channels.find((channel) =>{
+    return channel.nom === request.body.nomChannel
+  });
+  if(channel.participants.indexOf(note.emetteur)!==-1 && note.emetteur.points>=2)
+	{
+		store.state.messages.push(note);
+		store.state.channels[store.state.channels.indexOf(channel)].messages.push(note.id);
+		calculerPoints(note.emetteur);
+	}
+  response.send();
+}
+
+// user: User
+// userPseudo : String
+export function suspendUser(request: Request, response: Response){  
+  if(request.body.user.points===5)
+	{
+    const userToSuspend: User = store.state.users.find((user) =>{
+      return user.pseudo === request.body.userPseudo
+    });
+		userToSuspend.status="Suspendu";
+	}
+  response.send();
+}
+
+function calculerPoints(user: User){ 
+  var index:number = store.state.users.indexOf(user);
+	if(store.state.users[index].points<5)
+	{
+		store.state.users[index].points++;
+	}
+}
 
 export function ping(request: Request, response: Response){
   response.send({ ping: 'ok'})
